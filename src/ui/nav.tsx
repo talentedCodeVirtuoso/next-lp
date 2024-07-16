bff61bcf0b6a87ff1adc5083c99b22ad4cf51e17
talentedCodeVirtuoso <wiasdasdjaosdawd@gmail.com>
2024-07-16 11:26:41 -0700

    nav footer code

--- src/ui/nav.tsx
+++ src/ui/nav.tsx
@@ -6,7 +6,21 @@ const Nav = () => {
     <nav className="sticky top-0 flex items-center justify-between px-6 py-8 border-b border-gray-200 bg-white/50 backdrop-bluer-xl z-50">
       <div className="flex gax-x-7 items-center">
         <Logo />
+        <ul className="flex gap-x-2">
+          <li>
+            <Link href="#about">About</Link>
+          </li>
+          <li>
+            <Link href="#offer">Offer</Link>
+          </li>
+        </ul>
       </div>
+
+      <a href="mailto:contact@techstart.inc" className="font-medium">
+        Contact us
+      </a>
     </nav>
   );
 };
+
+export default Nav;
